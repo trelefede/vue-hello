399526516a3c15b2b598c7e15de1dce030a64376
TreleaniFederico <treleani.federico@gmail.com>
2022-09-12 17:14:10 +0200

    inside h1

--- js/script.js
+++ js/script.js
@@ -3,6 +3,6 @@ console.log('js ok');
 const app = new Vue({
     el: '#container',
     data: {
-        comunication: 'Dai che è venerdi!! ..'
+        comunication: 'Dai che è lunedì!! ..'
     }
 })
