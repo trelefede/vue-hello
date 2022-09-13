db51398afd24e590e579ee7f6e0d045ae66b17ae
TreleaniFederico <treleani.federico@gmail.com>
2022-09-13 09:49:03 +0200

    add image url to script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ console.log('js ok');
 const app = new Vue({
     el: '#container',
     data: {
-        comunication: 'Dai che è lunedì!! ..'
+        comunication: 'Dai che è lunedì!! ..',
+        infoImage: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop'
     }
 })
